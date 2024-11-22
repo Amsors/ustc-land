3e36b8680d79b8cdd59b01cf805970135dfcb1dd
amsors <amsors163@163.com>
2024-11-22 21:08:52 +0800

    完善json读取

--- src/cpp/readjson.cpp
+++ src/cpp/readjson.cpp
@@ -6,49 +6,89 @@
 #include <game/logic/cards.h>
 #include <game/logic/register.h>
 
+#include "spdlog/spdlog.h"
+
+std::string tryReadString(Json::Value &v,std::string key) {
+	if (v.isMember(key)) {
+		return v[key].asString();
+	}
+	SPDLOG_LOGGER_WARN(spdlog::get("readjson"), "key: {} not found", key);
+	return v[key].asString();
+}
+
+int tryReadInt(Json::Value &v, std::string key) {
+	if (v.isMember(key)) {
+		return v[key].asInt();
+	}
+	SPDLOG_LOGGER_WARN(spdlog::get("readjson"), "key: {} not found", key);
+	return v[key].asInt();
+}
+
+bool tryReadBool(Json::Value &v, std::string key) {
+	if (v.isMember(key)) {
+		return v[key].asBool();
+	}
+	SPDLOG_LOGGER_WARN(spdlog::get("readjson"), "key: {} not found", key);
+	return v[key].asBool();
+}
+
+double tryReadDouble(Json::Value &v, std::string key) {
+	if (v.isMember(key)) {
+		return v[key].asDouble();
+	}
+	SPDLOG_LOGGER_WARN(spdlog::get("readjson"), "key: {} not found", key);
+	return v[key].asDouble();
+}
+
 void readAttributeJson() {
 	std::ifstream inFile("rule/attribute.json");
 	Json::Reader reader;
 	Json::Value root;
 	reader.parse(inFile, root);
+
 	int attributeValueSum = root["attributeValueSum"].asInt();
-	Json::Value jValue = root["attributeValue"];
+	Json::Value jAttributeValue = root["attributeValue"];
 	for (int i = 0; i < attributeValueSum; i++) {
-		Json::Value single = jValue[i];
-		Attribute* att = new Attribute();
+		Json::Value jSingleAttributeValue = jAttributeValue[i];
+		Attribute* newAttribute = new Attribute();
 
-		att->isArray = 0;
-		att->name = single["name"].asString();
-		att->visibility = single["visibility"].asBool();
-		att->max = single["max"].asDouble();
-		att->initial = single["initial"].asDouble();
+		newAttribute->isArray = 0;
+		newAttribute->name = jSingleAttributeValue["name"].asString();
+		newAttribute->visibility = jSingleAttributeValue["visibility"].asBool();
+		newAttribute->max = jSingleAttributeValue["max"].asDouble();
+		newAttribute->initial = jSingleAttributeValue["initial"].asDouble();
 
-		att->showAttribute();
+		SPDLOG_LOGGER_TRACE(spdlog::get("readjson"), "read attribute. kind: {}, index: {}, name: {}",
+			"Value", i, jSingleAttributeValue["name"].asString());
+		//newAttribute->showAttribute();
 
-		reg.regAttribute.emplace(std::pair<std::string, Attribute*>(att->name, att));
+		reg.regAttribute.emplace(std::pair<std::string, Attribute*>(newAttribute->name, newAttribute));
 	}
 
 	int attributeArraySum = root["attributeArraySum"].asInt();
-	Json::Value jArray = root["attributeArray"];
+	Json::Value jAttributeArray = root["attributeArray"];
 	for (int i = 0; i < attributeArraySum; i++) {
-		Json::Value single = jArray[i];
-		if (single["arrayLengthStrategy"].asString() == "match") {
-			std::string keyTemp(single["arrayLengthMatchKey"].asString());
+		Json::Value jSingleAttributeArray = jAttributeArray[i];
+		if (jSingleAttributeArray["arrayLengthStrategy"].asString() == "match") {
+			std::string keyTemp(jSingleAttributeArray["arrayLengthMatchKey"].asString());
 			int length = reg.regValue[keyTemp];
-			Attribute* att = new Attribute();
+			Attribute* newAttribute = new Attribute();
 
-			att->isArray = 1;
-			att->name = single["name"].asString();
-			att->visibility = single["visibility"].asBool();
+			newAttribute->isArray = 1;
+			newAttribute->name = jSingleAttributeArray["name"].asString();
+			newAttribute->visibility = jSingleAttributeArray["visibility"].asBool();
 
 			for (int j = 0; j < length; j++) {
-				att->attributeArray.push_back(0);
+				newAttribute->attributeArray.push_back(0);
 			}
-			att->attributeArrayMatch = single["arrayLengthMatchKey"].asString();
+			newAttribute->attributeArrayMatch = jSingleAttributeArray["arrayLengthMatchKey"].asString();
 
-			att->showAttribute();
+			SPDLOG_LOGGER_TRACE(spdlog::get("readjson"), "read attribute. kind: {}, index: {}, name: {}",
+				"Array", i, jSingleAttributeArray["name"].asString());
+			//newAttribute->showAttribute();
 		}
 	}
+
 	inFile.close();
 	return;
 }
@@ -61,47 +101,85 @@ void readAdvancementJson() {
 	int advancementSum = root["advancementSum"].asInt();
 	Json::Value j = root["advancement"];
 	for (int i = 0; i < advancementSum; i++) {
-		Json::Value single = j[i];
-		Advancement* adv = new Advancement();
+		Json::Value singleAdvancement = j[i];
+		Advancement* newAdvancement = new Advancement();
 
-		adv->name = single["name"].asString();
+		newAdvancement->name = singleAdvancement["name"].asString();
 
-		int formulaNeededSum = single["formulaNeededSum"].asInt();
-		Json::Value jFormulaNeeded = single["formulaNeeded"];
+		int formulaNeededSum = singleAdvancement["formulaNeededSum"].asInt();
+		Json::Value jFormulaNeeded = singleAdvancement["formulaNeeded"];
 		for (int j = 0; j < formulaNeededSum; j++) {
-			adv->formulaNeeded.push_back(jFormulaNeeded[j].asString());
-		}
-		
-		int attributeValueNeededSum = single["attributeValueNeededSum"].asInt();
-		Json::Value jAttributeValueNeeded = single["attributeValueNeeded"];
-		for (int j = 0; j < attributeValueNeededSum; j++) {
-			Json::Value jtmp = jAttributeValueNeeded[j];
-			std::pair<double, double> ptmp(jtmp["upper"].asDouble(), jtmp["lower"].asDouble());
-			std::string stmp = jtmp["attributeName"].asString();
-			adv->attributeValueNeeded.emplace(std::pair<std::string, std::pair<double, double>>(
-				stmp, ptmp));
+			newAdvancement->formulaNeeded.push_back(jFormulaNeeded[j].asString());
 		}
 
-		int attributeArrayNeededSum = single["attributeArrayNeededSum"].asInt();
-		Json::Value jAttributeArrayNeeded = single["attributeArrayNeeded"];
+		int cardNeededSum = singleAdvancement["cardNeededSum"].asInt();
+		Json::Value jCardNeeded = singleAdvancement["cardNeeded"];
+		for (int j = 0; j < cardNeededSum; j++) {
+			newAdvancement->cardNeeded.push_back(jCardNeeded[j].asString());
+		}
+
+		int itemNeededSum = singleAdvancement["itemNeededSum"].asInt();
+		Json::Value jItemNeeded = singleAdvancement["itemNeeded"];
+		for (int j = 0; j < itemNeededSum; j++) {
+			newAdvancement->itemNeeded.push_back(jItemNeeded[j].asString());
+		}
+
+		int attributeValueNeededSum = singleAdvancement["attributeValueNeededSum"].asInt();
+		Json::Value jAttributeValueNeeded = singleAdvancement["attributeValueNeeded"];
+		for (int j = 0; j < attributeValueNeededSum; j++) {
+			Json::Value singleAttribute = jAttributeValueNeeded[j];
+			std::pair<double, double> rangetmp(singleAttribute["upper"].asDouble(), singleAttribute["lower"].asDouble());
+			std::string stmp = singleAttribute["attributeName"].asString();
+			newAdvancement->attributeValueNeeded.emplace(std::pair<std::string, std::pair<double, double>>(
+				stmp, rangetmp));
+		}
+
+		int attributeArrayNeededSum = singleAdvancement["attributeArrayNeededSum"].asInt();
+		Json::Value jAttributeArrayNeeded = singleAdvancement["attributeArrayNeeded"];
 		for (int j = 0; j < attributeArrayNeededSum; j++) {
-			Json::Value jtmp = jAttributeArrayNeeded[j];
-			std::string stmp = jtmp["attributeName"].asString();
+			Json::Value jSingleAttribute = jAttributeArrayNeeded[j];
+			std::string stmp = jSingleAttribute["attributeName"].asString();
 			std::string keytmp = reg.regAttribute[stmp]->attributeArrayMatch;
-			int restrictionSum = jtmp["restrictionSum"].asInt();
-			Json::Value res = jtmp["restriction"];
+			int restrictionSum = jSingleAttribute["restrictionSum"].asInt();
+			Json::Value jrestriction = jSingleAttribute["restriction"];
 			for (int k = 0; k < restrictionSum; k++) {
-				Json::Value singleres = res[k];
-				std::pair<double, double> resvalue(singleres["upper"].asDouble(), singleres["lower"].asDouble());
-				std::pair<int, std::pair<double, double>> resdetail(singleres["index"].asInt(), resvalue);
-				adv->attributeArrayNeeded.emplace(std::pair<std::string, std::pair<int, std::pair<double, double>>>(
-					stmp, resdetail
+				Json::Value singlerestriction = jrestriction[k];
+				std::pair<double, double> rangetmp(singlerestriction["upper"].asDouble(), singlerestriction["lower"].asDouble());
+				std::pair<int, std::pair<double, double>> detailtmp(singlerestriction["index"].asInt(), rangetmp);
+				newAdvancement->attributeArrayNeeded.emplace(std::pair<std::string, std::pair<int, std::pair<double, double>>>(
+					stmp, detailtmp
 				));
 			}
 		}
 		
-		reg.regAdvancement.emplace(std::pair<std::string, Advancement*>(adv->name, adv));
+		reg.regAdvancement.emplace(std::pair<std::string, Advancement*>(newAdvancement->name, newAdvancement));
 	}
+	inFile.close();
+	return;
+}
+
+void readCardSetJson() {
+	std::ifstream inFile("rule/cardset.json");
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(inFile, root);
+
+	int cardSetSum = root["cardSetSum"].asInt();
+	Json::Value jcardSet = root["cardSet"];
+	for (int i = 0; i < cardSetSum; i++) {
+		Json::Value jSingleCardSet = jcardSet[i];
+		Json::Value jCard = jSingleCardSet["card"];
+
+		CardSet* newCardSet = new CardSet;
+		for (int i = 0; i < root["cardNum"].asInt(); i++) {
+			//cs.getCardset().push(jCS[i].asString());
+			newCardSet->cardSet.emplace(jCard[i].asString());
+		}
+		reg.cardSetPtr.emplace(std::pair<std::string, CardSet*>(
+			jSingleCardSet["cardSetName"].asString(), newCardSet));
+
+	}
+
 	inFile.close();
 	return;
 }
@@ -113,22 +191,14 @@ void readFormulaJson() {
 	reader.parse(inFile, root);
 
 	int formulaSum = root["formulaSum"].asInt();
-	Json::Value jf = root["formula"];
+	Json::Value jFormula = root["formula"];
 	for (int i = 0; i < formulaSum; i++) {
-		Json::Value jfsingle = jf[i];
+		Json::Value jSingleFormula = jFormula[i];
 
-		if (reg.cardSetRegistered[jfsingle["cardSetName"].asString()] == false) {
-			reg.cardSetRegistered[jfsingle["cardSetName"].asString()] = true;
-			CardSet* newCardSet = new CardSet;
-			readCardSetJson(jfsingle, *newCardSet);
-			reg.cardSetPtr.emplace(std::pair<std::string, CardSet*>(
-				jfsingle["cardSetName"].asString(), newCardSet));
-		}
-
-		Formula* newformula = new Formula;
-		reg.formulaAttained[jfsingle["formulaName"].asString()] = false;
+		Formula* newFormula = new Formula;
+		reg.formulaAttained[jSingleFormula["formulaName"].asString()] = false;
 		reg.formulaPtr.emplace(std::pair<std::string, Formula*>(
-			jfsingle["formulaName"].asString(), newformula));
+			jSingleFormula["formulaName"].asString(), newFormula));
 		//TODO reward²¿·Ö
 
 	}
@@ -138,13 +208,37 @@ void readFormulaJson() {
 }
 
 void readSpotJSon() {
+	std::ifstream inFile("rule/spot.json");
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(inFile, root);
 
+	
+	inFile.close();
 }
 
 void readClassJson() {
+	std::ifstream inFile("rule/spot.json");
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(inFile, root);
 
+	reg.regValue.emplace(std::pair<std::string, int>("classSum", root["classSum"].asInt()));
+	Json::Value jClass = root["class"];
+	for (int i = 0; i < root["classSum"].asInt(); i++) {
+		Json::Value jSingleClass = jClass["class"];
+		reg.classNameToIndex.emplace(std::pair<std::string, int>(
+			jSingleClass["name"].asString(), jSingleClass["index"].asInt()));
+	}
+
+	inFile.close();
 }
 
 void readItemJson() {
+	std::ifstream inFile("rule/item.json");
+	Json::Reader reader;
+	Json::Value root;
+	reader.parse(inFile, root);
 
+	inFile.close();
 }
